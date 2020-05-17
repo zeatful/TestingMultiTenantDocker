010566af380b44e1c6fe2cf5a4655269f917ddc5
Eric Foster <efoster@harriscomputer.com>
2020-05-17 17:17:09 -0400

    add stages

--- qa.groovy
+++ qa.groovy
@@ -5,8 +5,10 @@ pipeline {
         REDIS_PORT = '6380'
     }
 
-    stage("Env Variables") {
-        sh "printenv"
-        echo "echo ${env.REDIS_PORT}"
+    stages {
+        stage("Env Variables") {
+            sh "printenv"
+            echo "echo ${env.REDIS_PORT}"
+        }
     }
 }
