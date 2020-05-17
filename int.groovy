8c0e59b3e74da8a85f678a84f34ce4b1edbdb916
Eric Foster <efoster@harriscomputer.com>
2020-05-17 18:11:22 -0400

    update int.groovy to be generic pipeline

--- int.groovy
+++ int.groovy
@@ -1,17 +1,108 @@
 pipeline {
-    agent any
-
-    environment {
-        NAME = "INT"
-        REDIS_PORT = "6379"
+    agent { label 'slave-1' }
+    
+    parameters {
+        choice (
+            name: 'ENVIRONMENT_NAME',
+            choices: ['int', 'qa', 'demo', 'stage', 'prod'],
+            description: 'Choose which environment to deploy from master'
+        )
     }
 
     stages {
-        stage("Env Variables") {
+        stage ('Stop Running Services') {
             steps {
-                echo "${env.REDIS_PORT}"
-                sh "docker-compose -p ${env.NAME} up -d"
-            }                        
+                sh "docker-compose -f docker-compose.managed.yml -p ${params.ENVIRONMENT_NAME} down --remove-orphans"
+            }
+        }
+
+        stage ('Deploy to INT') {
+            when {
+                anyOf {
+                    expression { params.ENVIRONMENT_NAME == 'INT' }
+                }
+            }
+
+            environment {
+                REDIS_PORT = "6379"
+            }
+
+            steps {
+                sh "docker-compose -f docker-compose.managed.yml -p ${params.ENVIRONMENT_NAME} up --force-recreate -d"
+            }
+        }
+
+        stage ('Deploy to QA') {
+            when {
+                anyOf {
+                    expression { params.ENVIRONMENT_NAME == 'QA' }
+                }
+            }
+
+            environment {
+                REDIS_PORT = "6380"
+            }
+
+            steps {
+                sh "docker-compose -f docker-compose.managed.yml -p ${params.ENVIRONMENT_NAME} up --force-recreate -d"
+            }
+        }
+
+        stage ('Deploy to Q') {
+            when {
+                anyOf {
+                    expression { params.ENVIRONMENT_NAME == 'DEMO' }
+                }
+            }
+
+            environment {
+                REDIS_PORT = "6381"
+            }
+
+            steps {
+                sh "docker-compose -f docker-compose.managed.yml -p ${params.ENVIRONMENT_NAME} up --force-recreate -d"
+            }            
+        }
+
+        stage ('Set STAGE Environment Variables') {
+            when {
+                anyOf {
+                    expression { params.ENVIRONMENT_NAME == 'STAGE' }
+                }
+            }
+
+            environment {
+                REDIS_PORT = "6382"
+            }
+
+            steps {
+                sh "docker-compose -f docker-compose.managed.yml -p ${params.ENVIRONMENT_NAME} up --force-recreate -d"
+            }            
+        }
+
+        stage ('Set PROD Environment Variables') {
+            when {
+                anyOf {
+                    expression { params.ENVIRONMENT_NAME == 'PROD' }
+                }
+            }
+
+            environment {
+                REDIS_PORT = "6383"
+            }
+
+            steps {
+                sh "docker-compose -f docker-compose.managed.yml -p ${params.ENVIRONMENT_NAME} up --force-recreate -d"
+            }            
+        }
+
+        stage('Perform Docker Cleanup'){
+            steps {
+            // Remove dangling images and networks
+            sh 'docker image prune -f'
+            sh 'docker image prune -fa'
+            sh 'docker network prune -f'
+            }
         }
     }
 }
