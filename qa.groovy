fd83ae34a60f463cd12cc57b22f82aa3c29463b2
Eric Foster <efoster@harriscomputer.com>
2020-05-17 17:12:36 -0400

    add pipeline wrapper to jenkinsfile

--- qa.groovy
+++ qa.groovy
@@ -1,4 +1,5 @@
-node('slave1') {
+pipeline {
+    agent any
 
     environment {
         REDIS_PORT = '6380'
@@ -8,15 +9,4 @@ node('slave1') {
         sh "printenv"
         echo "echo ${env.REDIS_PORT}"
     }
-
-    // stage('print redis port'){        
-    // sh 'docker-compose -p QA up --force-recreate -d'
-    // }
-
-    // stage('Perform Docker Cleanup'){
-    //     // Remove dangling images and networks
-    //     sh 'docker image prune -f'
-    //     sh 'docker image prune -fa'
-    //     sh 'docker network prune -f'
-    // }
 }
