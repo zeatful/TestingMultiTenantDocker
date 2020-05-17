06be5a35241fd73c362976c49e004c940d6605b3
Eric Foster <efoster@harriscomputer.com>
2020-05-17 16:48:28 -0400

    remove stages tag

--- qa.groovy
+++ qa.groovy
@@ -4,21 +4,19 @@ node('slave1') {
         REDIS_PORT = '6380'
     }
 
-    stages {
-        stage("Env Variables") {
-            sh "printenv"
-            echo "echo ${env.REDIS_PORT}"
-        }
-
-        // stage('print redis port'){        
-        // sh 'docker-compose -p QA up --force-recreate -d'
-        // }
-
-        // stage('Perform Docker Cleanup'){
-        //     // Remove dangling images and networks
-        //     sh 'docker image prune -f'
-        //     sh 'docker image prune -fa'
-        //     sh 'docker network prune -f'
-        // }
+    stage("Env Variables") {
+        sh "printenv"
+        echo "echo ${env.REDIS_PORT}"
     }
+
+    // stage('print redis port'){        
+    // sh 'docker-compose -p QA up --force-recreate -d'
+    // }
+
+    // stage('Perform Docker Cleanup'){
+    //     // Remove dangling images and networks
+    //     sh 'docker image prune -f'
+    //     sh 'docker image prune -fa'
+    //     sh 'docker network prune -f'
+    // }
 }
