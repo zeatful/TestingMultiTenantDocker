dc9330741ae26c3e07969a53b32a143c7dd15aac
Eric Foster <efoster@harriscomputer.com>
2020-05-17 17:21:46 -0400

    clean up steps section

--- qa.groovy
+++ qa.groovy
@@ -1,19 +1,14 @@
 pipeline {
     agent any
 
-    environment {
-        REDIS_PORT = '6380'
-    }
-
     stages {
         stage("Env Variables") {
+            environment {
+                REDIS_PORT = "6380"
+            }
+
             steps {
-                step ('print environment variables') {
-                    sh "printenv"
-                }
-                step ('print REDIS_PORT') {
-                    echo "${env.REDIS_PORT}"
-                } 
+                echo "${env.REDIS_PORT}"
             }                        
         }
     }
